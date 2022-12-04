74eaa739571193103c06c17d86fdf31343e53400
5erzhant <5erzhant@mail.ru>
2022-12-04 14:23:29 +0300

    HW 11-1 Deadlock

--- src/com/urise/webapp/MainDeadLock.java
+++ src/com/urise/webapp/MainDeadLock.java
@@ -5,24 +5,22 @@ public class MainDeadLock {
     public static void main(String[] args) {
         MainDeadLock deadLock1 = new MainDeadLock();
         MainDeadLock deadLock2 = new MainDeadLock();
-        Thread t1 = new Thread(() -> {
-            synchronized (deadLock1) {
-                Thread.yield();
-                synchronized (deadLock2) {
-                    System.out.println("Deadlock");
-                }
-            }
-        });
 
-        Thread t2 = new Thread(() -> {
-            synchronized (deadLock2) {
-                Thread.yield();
-                synchronized (deadLock1) {
-                    System.out.println("Deadlock");
+        deadLock(deadLock1, deadLock2);
+        deadLock(deadLock2, deadLock1);
+    }
+
+    public static void deadLock(MainDeadLock dl1, MainDeadLock dl2) {
+        new Thread(() -> {
+            synchronized (dl1) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (dl2) {
                 }
             }
-        });
-        t1.start();
-        t2.start();
+        }).start();
     }
 }
